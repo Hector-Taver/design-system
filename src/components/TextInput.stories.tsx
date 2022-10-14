e2d79831b2f9f9c76f2aa3e0f41c14b84454e2f4
Hector van Tol Taver <hectorvttaver@gmail.com>
2022-10-14 16:10:14 -0300

    Initial commit

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,44 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Text } from "./Text";
+import { EnvelopeSimple, Lock } from "phosphor-react";
+
+import { TextInput, TextInputRootProps } from "./TextInput";
+
+export default {
+  title: "Components/TextInput",
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+        <EnvelopeSimple />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="Type your e-mail address" />,
+    ],
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<TextInputRootProps>;
+
+export const Default: StoryObj<TextInputRootProps> = {};
+
+export const PasswordInput: StoryObj<TextInputRootProps> = {
+  args: {
+    children: [
+      <TextInput.Icon>
+        <Lock />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder="*********" />,
+    ],
+  },
+};
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: <TextInput.Input placeholder="Input without icon" />,
+  },
+};
